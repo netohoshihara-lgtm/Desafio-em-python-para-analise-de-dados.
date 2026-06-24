85f51a18809052a921b9e5663435a55a6c33e022
Neto Hoshihara <netohoshihara@gmail.com>
2026-06-23 21:29:00 -0300

    Desafio 01

---
+++ Desafio01.py
@@ -0,0 +1,25 @@
+ #Desafio 1 – Análise de Vendas
+ #lista com as vendas de uma loja durante 10 dias:
+
+vendas = [120, 250, 180, 300, 150, 400, 220, 190, 350, 280]
+
+#Tarefas:
+#1. Calcule o total de vendas.
+total_de_vendas = sum(vendas)
+print('O total de vendas é:',total_de_vendas)
+
+#2. Calcule a média de vendas.
+media_de_vendas = sum(vendas) /len(vendas)
+print('A média de vendas é:', media_de_vendas)
+
+#3. Descubra a maior venda.
+maior_venda = max(vendas)
+print('A maior venda efetuada é:',maior_venda)
+
+#4. Descubra a menor venda.
+menor_venda = min(vendas)
+print('A menor venda efetuada é:', menor_venda)
+
+#5. Mostre quantos dias tiveram vendas acima de R$ 250.
+dias_acima = len([acima for acima in vendas if acima >250])
+print('Este é o total de vendas acima de 250:', dias_acima)
